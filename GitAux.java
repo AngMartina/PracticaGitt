f7bc952f4ed8d0e4b706aa3dbecdbcd2e0ce14ee
AngMartina <apadronpol@gmail.com>
2016-04-07 19:52:04 +0200

    Hemos añadido comentarios y lineas

--- GitAux.java
+++ GitAux.java
@@ -1,5 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
+/* * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package prgitejemplo;
@@ -9,15 +8,16 @@ package prgitejemplo;
  * @author bertoa
  */
 public class GitAux {
-    
     public void metodoAlumno1(){
-        System.out.println("metodo 1");
+    	System.out.println("metodo 1");
     }
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
     }
     
+	//Añadimos comentario para pruebas
+
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
     }
